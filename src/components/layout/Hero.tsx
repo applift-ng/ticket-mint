74fec0472d7e3cdea52354462a8f602cd9b2e2fd
Victor Lawrence <viclawrence99@gmail.com>
2024-03-27 01:30:31 +0100

    little responsiveness

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export const Hero = () => {
   return (
     <div className="bg-hero-image flex items-center w-full h-[88vh] px-10 bg-bottom bg-cover bg-no-repeat">
-      <h1 className="text-white w-1/3 flex flex-col  md:text-5xl font-bold">
+      <h1 className="text-white md:w-1/3 flex text-center md:text-left flex-col text-5xl font-bold">
         <span className="font-julee">Find your</span>
-        <span className="text-verdant text-9xl -mt-2 font-extrabold">
+        <span className="text-verdant text-7xl md:text-9xl -mt-2 font-extrabold">
           SQUAD
         </span>
       </h1>
